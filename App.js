eb8604a319c3c9e09307c8bb48ac33a82139bfe9
Facu <facuparra96@gmail.com>
2021-11-09 22:21:54 -0300

    Lista facundo parra

--- App.js
+++ App.js
@@ -1,21 +1,169 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  TextInput,
+  Button,
+  Text,
+  View,
+  FlatList,
+  Modal,
+} from 'react-native';
 
 export default function App() {
+  const [inputText, setInputText] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [itemList, setItemList] = useState([]);
+
+  const [itemSelected, setItemSelected] = useState({});
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleChangeText = (text) => {
+    setInputText(text)
+    setInputError('');
+  };
+
+  const handleAddItem = () => {
+    if (inputText) {
+      setItemList([
+        ...itemList,
+        {
+          id: Math.random().toString(),
+          value: inputText,
+        },
+      ]);
+      setInputText('');
+      setInputError('');
+    } else {
+      setInputError('Required');
+    }
+  }
+
+  const handleConfirmDelete = () => {
+    const id = itemSelected.id;
+    setItemList(itemList.filter(item => item.id !== id));
+    setModalVisible(false);
+    setItemSelected({});
+  }
+
+  const handleModal = id => {
+    setItemSelected(itemList.find(item => item.id === id));
+    setModalVisible(true);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hola, Coder!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Agregar item"
+          style={styles.input}
+          onChangeText={handleChangeText}
+          value={inputText}
+        />
+        <Button
+          title="ADD"
+          color="#3D9970"
+          onPress={handleAddItem}
+        />
+      </View>
+      <Text style={styles.inputError}>{inputError}</Text>
+      <FlatList
+        data={itemList}
+        renderItem={data => {
+          return (
+            <View style={[styles.item, styles.shadow]}>
+              <Text>{data.item.value}</Text>
+              <Button
+                title="X"
+                color="#AAAAAA"
+                onPress={() => handleModal(data.item.id)}
+              />
+            </View>
+          );
+        }}
+        keyExtractor={item => item.id}
+      />
+      <Modal animationType="slide" visible={modalVisible} transparent>
+        <View style={styles.modalContainer}>
+          <View style={[styles.modalContent, styles.shadow]}>
+            <Text style={styles.modalMessage}>¿Está seguro que desea borrar?</Text>
+            <Text style={styles.modalTitle}>{itemSelected.value}</Text>
+            <View>
+              <Button
+                onPress={handleConfirmDelete}
+                title="CONFIRMAR"
+              />
+            </View>
+          </View>
+        </View>
+      </Modal>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
+    padding: 30,
+    backgroundColor: '#F0F0F0',
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  input: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    width: 200,
+  },
+  inputError: {
+    color: 'red',
+  },
+  items: {
+    marginTop: 20,
+  },
+  item: {
+    padding: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 5,
+    backgroundColor: 'white',
+  },
+  modalContainer: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  modalContent: {
+    padding: 30,
+    backgroundColor: 'white',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalMessage: {
+    fontSize: 18,
+  },
+  modalTitle: {
+    fontSize: 30,
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  }
 });
